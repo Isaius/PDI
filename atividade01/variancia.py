18f840eee28ba26f6f81ad4a8f3a2dc8ff9b173a
Isaias <isaigrec@gmail.com>
2019-08-17 17:17:26 -0300

    acrescentar resultados no relatorio

--- atividade01/variancia.py
+++ atividade01/variancia.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 
 # abrindo a imagem 1 em forma de matriz
-img = cv2.imread('WashingtonDC_01.TIF', 0)
+img = cv2.imread('WashingtonDC_02.TIF', 0)
 # img.shape retorna uma tupla com (altura, largura) ou (linhas, colunas)
 dimensoes = img.shape
 # guardando as dimensoes para serem usadas para percorrer a matriz

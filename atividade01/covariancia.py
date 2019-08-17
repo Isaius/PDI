2e6119b16c14c4281039500c05b4abea5a086ab0
Isaias <isaigrec@gmail.com>
2019-08-17 16:17:02 -0300

    incluir calculo da covariancia

--- atividade01/covariancia.py
+++ atividade01/covariancia.py
@@ -4,9 +4,53 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('WashingtonDC_01.TIF', 0)
+img1 = cv2.imread('WashingtonDC_01.TIF', 0)
 # img.shape retorna uma tupla com (altura, largura) ou (linhas, colunas)
-dimensoes = img.shape
+dimensoes1 = img1.shape
 # guardando as dimensoes para serem usadas para percorrer a matriz
-largura = dimensoes[1]
-altura = dimensoes[0]
+largura1 = dimensoes1[1]
+altura1 = dimensoes1[0]
+
+img2 = cv2.imread('WashingtonDC_02.TIF', 0)
+# img.shape retorna uma tupla com (altura, largura) ou (linhas, colunas)
+dimensoes2 = img2.shape
+# guardando as dimensoes para serem usadas para percorrer a matriz
+largura2 = dimensoes2[1]
+altura2 = dimensoes2[0]
+
+if(largura1 == largura2 and altura1 == altura2):
+    print("imagens de mesma dimensao")
+    altura = altura1
+    largura = largura1
+
+somatorio = 0
+media1 = 0
+# percorrendo a matriz pixel por pixel e somando o valor de cinza
+for i in range(altura):
+    for k in range(largura):
+        somatorio += img1[i, k]
+# media da imagem
+media1 = somatorio/img1.size
+
+media2 = 0
+somatorio = 0
+# percorrendo a matriz pixel por pixel e somando o valor de cinza
+for i in range(altura):
+    for k in range(largura):
+        somatorio += img2[i, k]
+# media da imagem
+media2 = somatorio/img2.size
+
+somatorio = 0
+# algora calculando a covariancia entre as duas imagens
+for i in range(altura):
+    for k in range(largura):
+        # desvio de cinza da imagem 1
+        desvio1 = img1[i, k] - media1
+        # desvio de cinza da imagem 2
+        desvio2 = img2[i, k] - media2
+        #somatorio do produto entre os dois desvios
+        somatorio += desvio1*desvio2
+
+covariancia = somatorio/(img1.size-1)
+print("A covariancia entre as duas imagens é: " + str(covariancia))

db897d689c64dd576cd2c1e67d658c4d84c597b0
Isaias <isaigrec@gmail.com>
2019-08-17 14:42:50 -0300

    incluir media da imagem 1

--- atividade01/media.py
+++ atividade01/media.py
@@ -1,8 +1,21 @@
 import numpy as np
 import cv2
 
+# abrindo a imagem 1 em forma de matriz
 img = cv2.imread('WashingtonDC_01.TIF', 0)
+# img.shape retorna uma tupla com (altura, largura) ou (linhas, colunas)
+dimensoes = img.shape
+# guardando as dimensoes para serem usadas para percorrer a matriz
+largura = dimensoes[1]
+altura = dimensoes[0]
+# variaveis resultados
+media = 0
+somatorio = 0
+# percorrendo a matriz pixel por pixel e somando o valor de cinza
+for i in range(altura-1):
+    for k in range(largura-1):
+        somatorio += img[i, k]
 
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print("Somatorio niveis decinza: " str(somatorio))
+media = somatorio/img.size
+print("Media da imagem: " str(media))

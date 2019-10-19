2c0d66ba4700ae4ec8a08115b4494e53154036e1
alvaro-ian <alvaro-ian@hotmail.com>
2019-10-19 09:32:50 -0300

    Finalizar problema 1

--- atividade03/problema-1.py
+++ atividade03/problema-1.py
@@ -1,1 +1,18 @@
 import cv2
+
+img = cv2.imread("morfologia.png")
+cv2.imshow("Imagem original", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+img1 = cv2.dilate(img, None, iterations=1)
+cv2.imshow("Imagem dilatada...", img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+img2 = cv2.erode(img1, None, iterations=1)
+cv2.imshow("... depois erodida", img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("output\problema-1.png", img2)
